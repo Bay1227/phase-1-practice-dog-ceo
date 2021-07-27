c3d9908ee5c86854694ec429ff3b4110df65b734
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-07-27 12:03:00 -0700

    commit tree 1b97ff86fa5d103bf43ed4397fedbf7d9226c55f

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
+
+function loadImage() {
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(img => {
+        img.message.forEach(image => appendImage(image))
+    })
+}
+function appendImage(picUrl) {
+    let container = document.querySelector('#dog-image-container');
+    let image = document.createElement('img');
+    image.src = picUrl;
+    image.width = 600
+    image.height = 500
+    container.appendChild(image);
+
+}
+
+function loadBreed(){
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(brd => {
+           breeds = Object.keys(brd.message) 
+           console.log(breeds)
+          
+            
+    })
+}
+//  const selector = document.querySelector("#breed-dropdown")
+//  selector.addEventListener("change", (e) => {
+//      const letter = e.target.value
+//      const breeds = document.querySelectorAll(li.dog-breed)
+//     })
+// }
+//function createNewList()
+
+
+//function appendBreed(){
+
+//}
+
+loadBreed()
